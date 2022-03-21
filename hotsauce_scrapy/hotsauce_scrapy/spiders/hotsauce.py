8dea52e89d14b77239dca2803f3fc259cb76f9a4
betza <betzabeth.btzth@gmail.com>
2022-03-21 16:27:38 -0400

    se logra trinidad

--- hotsauce_scrapy/hotsauce_scrapy/spiders/hotsauce.py
+++ hotsauce_scrapy/hotsauce_scrapy/spiders/hotsauce.py
@@ -2,7 +2,7 @@ from pathlib import Path, PurePath
 import scrapy
 import csv
 
-#nextbuttom = response.xpath('//div[@class="pagination"]/ul[@class="pagination-list"]/li[@class="pagination-item pagination-item--next"]/a[@class="pagination-link"]/@href').get()
+#nextbuttom = response.xpath('//div[@class="pagination"]/ul[@class="pagination-list"]/li[@class="pagination-item pagination-item--next"]/a[@class="pagination-link"]/@href').extract_first()
 class SpiderHotSauce(scrapy.Spider):
 
     name = 'hotsauce'
@@ -11,15 +11,11 @@ class SpiderHotSauce(scrapy.Spider):
         'https://www.hotsauce.com/'
     ]
 
-    custom_settings = {
-        'FEED_EXPORT_ENCODING': 'utf-8',
-        'ROBOTSTXT_OBEY': False
-    }
+    
 
     def parseWrite_AllViews(self, response, **kwargs):
-        page = kwargs['page']
         csvFile = kwargs['csvFile']
-
+        
         name= response.xpath('//div[@class="card-body"]/h4/a/text()').getall()
         price = response.xpath('//div[@class="price-section price-section--withoutTax "]/span/text()').getall()
         link = response.xpath('//div[@class="card-body"]/h4/a/@href').getall()
@@ -27,23 +23,26 @@ class SpiderHotSauce(scrapy.Spider):
         priceWithoutax = list(filter(lambda x: (price.index(x)%2!=0),price))
         priceNormal = list(filter(lambda x: (price.index(x)%2==0),price))
 
-        row= [[name[i], priceNormal[i], priceWithoutax[i],branch[i],link[i]] for i in range(0,len(name)-1) ]
+
+        try:
+            row= [[name[i], priceNormal[i], priceWithoutax[i],branch[i],link[i]] for i in range(0,len(name)-1) ]
+            writer = csv.writer(csvFile)
+            writer.writerows(row)
+        except:
+            print("SE ESCONETO")
+            
+
+        if response.xpath('//div[@class="price-section price-section--withoutTax "]/span[@data-product-rrp-without-tax]/text()').getall() == []:
+            price = response.xpath('//div[@class="price-section price-section--withoutTax "]/span[@data-product-rrp-without-tax]/text()')
+            row= [[name[i], price[i],branch[i],link[i]] for i in range(0,len(name)-1) ]
         
-        writer = csv.writer(csvFile)
-        writer.writerows(row)
-        page+=1
-        pagestr = "?sort=alphaasc&page=" + str(page)
-        next_page_button_link= response.urljoin(pagestr)
+       
+        
+        next_page_button_link = response.xpath('//div[@class="pagination"]/ul[@class="pagination-list"]/li[@class="pagination-item pagination-item--next"]/a[@class="pagination-link"]/@href').extract_first()
 
-        if (next_page_button_link.find("?sort=alphaasc&page=")!= -1) and page >= 3:
-            lastpage = "page=" + str(page-1)
-            thispage = "page=" + str(page)
-            pagestr =  next_page_button_link.replace(lastpage, thispage)
-            next_page_button_link= response.urljoin(pagestr)
-     
-
-        if row != []:
-            yield response.follow(next_page_button_link, callback = self.parseWrite_AllViews, cb_kwargs={'csvFile': csvFile, 'page':page})
+        if next_page_button_link is not None:
+        
+            yield response.follow(next_page_button_link, callback = self.parseWrite_AllViews, cb_kwargs={'csvFile': csvFile})
                  
         
 
@@ -55,16 +54,17 @@ class SpiderHotSauce(scrapy.Spider):
 
         
         count = 0
-        page = 1
+        
         if allViews_links == []:
             csvFile= open(f'{ruta}/{"sjnsdkjcsd"}.csv', 'w')
         else:
             for link in allViews_links:
+                allViews_names[count] =allViews_names[count].replace("/", "-")
                 csvFile =  open(f'{ruta}/{allViews_names[count]}.csv', 'w') 
                 header = ['Name', 'Price', 'Price witout Tax', 'Brand', 'Link']
                 writer = csv.writer(csvFile)
                 writer.writerow(header)
-                yield response.follow(link, callback = self.parseWrite_AllViews, cb_kwargs={'csvFile': csvFile, 'page':page, 'links': link})
+                yield response.follow(link, callback = self.parseWrite_AllViews, cb_kwargs={'csvFile': csvFile})
                 count+=1
 
 
@@ -78,9 +78,9 @@ class SpiderHotSauce(scrapy.Spider):
         ruta = PurePath.joinpath(Path.cwd(), 'Main Menu')
 
         for link in links_mainMenu: 
-
-            PurePath.joinpath(ruta , mainMenu_nameList[count]).mkdir(parents = True)
-            rutaCarpetas = PurePath.joinpath(ruta, mainMenu_nameList[count])
+            if count < len(mainMenu_nameList):
+                PurePath.joinpath(ruta , mainMenu_nameList[count]).mkdir(parents = True)
+                rutaCarpetas = PurePath.joinpath(ruta, mainMenu_nameList[count])
             
             yield response.follow(link, callback = self.parseAllViews, cb_kwargs={'rutaAllView': rutaCarpetas})
             count+=1
