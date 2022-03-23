1b63aa4d3a6f6d69d074a7dbe0ec1069f9f22a0b
betza <betzabeth.btzth@gmail.com>
2022-03-22 23:38:19 -0400

    Faltan algunas carpetas

--- hotsauce_scrapy/hotsauce_scrapy/spiders/hotsauce.py
+++ hotsauce_scrapy/hotsauce_scrapy/spiders/hotsauce.py
@@ -1,7 +1,8 @@
+from itertools import count
 from pathlib import Path, PurePath
 import scrapy
 import csv
-import panda as pd
+import pandas as pd
 
 
 #nextbuttom = response.xpath('//div[@class="pagination"]/ul[@class="pagination-list"]/li[@class="pagination-item pagination-item--next"]/a[@class="pagination-link"]/@href').extract_first()
@@ -15,64 +16,89 @@ class SpiderHotSauce(scrapy.Spider):
 
     
 
+
     def parseWrite_AllViews(self, response, **kwargs):
-        csvFile = kwargs['csvFile']
-        file = kwargs['file']
-        row = pd.DataFrame()
+      
+        filePath = kwargs['filePath']
+        
+        count = kwargs['numero']
         
         name= response.xpath('//div[@class="card-body"]/h4/a/text()').getall()
-        price = response.xpath('//div[@class="price-section price-section--withoutTax "]/span/text()').getall()
+        priceNormal= response.xpath('//div[@class="price-section price-section--withoutTax "]/span[@data-product-rrp-without-tax]/text()').getall()
+        priceWithoutax = response.xpath('//div[@class="price-section price-section--withoutTax "]/span[@data-product-price-without-tax]/text()').getall()
         link = response.xpath('//div[@class="card-body"]/h4/a/@href').getall()
         branch = response.xpath('//div[@class="brand-rating"]//p[@class="card-text" and @data-test-info-type="brandName"]/text()').getall()
-        priceWithoutax = list(filter(lambda x: (price.index(x)%2!=0),price))
-        priceNormal = list(filter(lambda x: (price.index(x)%2==0),price))
+        file = {'name':name, 'priceNormal': priceNormal, 'priceWithoutax': priceWithoutax, 'branch':branch, 'link':link}
+        for i in range(0, len(name)-1):
+            itera = response.xpath(f'//li[{str(i+1)}]//div[@class="brand-rating"]//p[@class="card-text" and @data-test-info-type="brandName"]/text()').getall()
+            iteraPrice = response.xpath(f'//li[{str(i+1)}]//div[@class="price-section price-section--withoutTax "]/span[@data-product-rrp-without-tax]/text()').getall()
+            if itera == []:
+                branch.insert(i, "NONE")
+            if priceNormal ==[]:
+                file = {'name':name,'priceNormal': 'NONE', 'priceWithoutax': priceWithoutax, 'branch':branch, 'link':link}
+            elif iteraPrice == []:
+                priceNormal.insert(i, "NONE")
 
-        error = response.xpath('//div[@class="price-section price-section--withoutTax "]/span[@data-product-rrp-without-tax]/text()').getall() == []
+        
+        numerito = int(len(filePath)-1)
+        nameFile =filePath[:numerito] + '.csv'
+         
         try:
-            if error:
-                price = response.xpath('//div[@class="price-section price-section--withoutTax "]/span[@data-product-rrp-without-tax]/text()')
-                header = ['Name', 'Price', 'Brand', 'Link']
-                writer = csv.writer(csvFile)
-                writer.writerow(header)
-                row= [[name[i], price[i],branch[i],link[i]] for i in range(0,len(name)-1) ]
-                writer.writerows(row)
+            pandafile = pd.DataFrame(file,columns=['name', 'priceNormal', 'priceWithoutax', 'branch', 'link']) 
+
+            if count == 0:
+                pandafile.to_csv(nameFile,mode='a',header=True, index=False)
+                count = 1
             else:
-                row= [[name[i], priceNormal[i], priceWithoutax[i],branch[i],link[i]] for i in range(0,len(name)-1) ]
-                header = ['Name', 'Price', 'Price witout Tax', 'Brand', 'Link']
-                writer = csv.writer(csvFile)
-                writer.writerow(header)
-                writer.writerows(row)
-        except:
-            
-            file.write("link" + str(response) +"\n")
-            print("SE ESCONETO")
+                pandafile.to_csv(nameFile,mode='a',header=False,index=False)
+        except ValueError as ve:
+            print('\n\n')
+            s =[len(file['name']),len(file['priceNormal']),len(file['priceWithoutax']),len(file['branch']),len(file['link'])]
+            errores = open('/home/betza/Python/Hotsauce_Scrapy/hotsauce_scrapy/veamos.txt', 'a')
+            errores.write(str(s) +" " + str(ve) + " "+ str(response) +'\n')
+           
+            print(ve)
+     
+
+    
        
         
         next_page_button_link = response.xpath('//div[@class="pagination"]/ul[@class="pagination-list"]/li[@class="pagination-item pagination-item--next"]/a[@class="pagination-link"]/@href').extract_first()
 
         if next_page_button_link is not None:
         
-            yield response.follow(next_page_button_link, callback = self.parseWrite_AllViews, cb_kwargs={'csvFile': csvFile,'file':file})
+            yield response.follow(next_page_button_link, callback = self.parseWrite_AllViews, cb_kwargs={'filePath': filePath, 'numero':count})
+            
                  
         
 
 
     def parseAllViews(self, response, **kwargs):
         ruta = kwargs[ 'rutaAllView']
+        folderName = kwargs['folderName']
         allViews_links =  response.xpath('//div[@class="sidebarBlock"]//li[@class="navList-item"]/a/@href').getall()
         allViews_names =  response.xpath('//div[@class="sidebarBlock"]//li[@class="navList-item"]/a/text()').getall()
-
-        
+        numero = 0
+         
         count = 0
-        file = open("/home/betza/Python/Hotsauce_Scrapy/hotsauce_scrapy/filename2.txt", "w")
-        if allViews_links == []:
-            csvFile= open(f'{ruta}/{"sjnsdkjcsd"}.csv', 'w')
+      
+        if allViews_links == [] :
+            
+            filePath = str(ruta)+ str(folderName) + '/'
+            try:
+                yield response.follow(response, callback = self.parseWrite_AllViews, cb_kwargs={'filePath':  filePath, 'numero':numero})
+            except TypeError as te:
+                print('\n\n')
+                print(te)
+                print(filePath)
+                print(response)
         else:
             for link in allViews_links:
-                allViews_names[count] =allViews_names[count].replace("/", "-")
-                csvFile =  open(f'{ruta}/{allViews_names[count]}.csv', 'w') 
-                
-                yield response.follow(link, callback = self.parseWrite_AllViews, cb_kwargs={'csvFile': csvFile, 'file':file})
+                allViews_names[count] = allViews_names[count].replace("/", "-")
+                filePath = str(ruta) + allViews_links[count].replace("https://www.hotsauce.com/", '/')
+         
+         
+                yield response.follow(link, callback = self.parseWrite_AllViews, cb_kwargs={'filePath':  filePath, 'numero':numero})
                 count+=1
 
 
@@ -80,7 +106,7 @@ class SpiderHotSauce(scrapy.Spider):
         links_mainMenu = response.xpath('//ul[@class="MainMenu"]/li/a/@href').getall()
         mainMenu_nameList = response.xpath('//ul[@class="MainMenu"]/li/a[@href]/text()').getall()
         count = 0
-
+        
 
         Path('Main Menu').mkdir(exist_ok=True)
         ruta = PurePath.joinpath(Path.cwd(), 'Main Menu')
@@ -89,8 +115,10 @@ class SpiderHotSauce(scrapy.Spider):
             if count < len(mainMenu_nameList):
                 PurePath.joinpath(ruta , mainMenu_nameList[count]).mkdir(parents = True)
                 rutaCarpetas = PurePath.joinpath(ruta, mainMenu_nameList[count])
+                folderName = mainMenu_nameList[count]
+
             
-            yield response.follow(link, callback = self.parseAllViews, cb_kwargs={'rutaAllView': rutaCarpetas})
+            yield response.follow(link, callback = self.parseAllViews, cb_kwargs={'rutaAllView': rutaCarpetas, 'folderName':folderName})
             count+=1
 
     
